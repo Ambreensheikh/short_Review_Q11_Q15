8686877d8993e3e626d8f251bb5dab188b06401b
Ambreen Sheikh <ambreensheikh900@gmail.com>
2024-06-10 01:10:02 +0500

    first commit

--- app.ts
+++ app.ts
@@ -1,37 +1,21 @@
-//Q_11
-let members:string[] = ["Ali","Sana","Hashir","Mamu"];
-for(let i=0; i< members.length; i++){
-   // console.log(members[i]);  
-}
-//Q_12
-let members1:string[] = ["Ali","Sana","Hashir","Mamu"];
- let message:string = `Kal birthday he kis ki !`;
- for(let i = 0; i< members.length; i++){
-    //console.log(message + members[i]);   
- }
- //Q_13
- let names:string[] = ["Car", "Bus","Truck"];
- let message1:string = `I like to ride on my !`
- for(let i = 0; i<names.length; i++){
- console.log(message1 + names[i]);}
- 
- //Q_14
- //let guest_list:string[] = ["Ali","Sana","Hashir","Mamu"];
- //for(let i=0; i< guest_list.length; i++){
-    //console.log(`Dear Sir/ Madam`+guest_list[i]+ `\nWe are really pleased to invite you in dinner!\n\n Thank You\n`);  
-// }
 
- //Q_15
- let guest_list:string[] = ["Ali","Sana","Hashir","Mamu"];
- for(let i=0; i< guest_list.length; i++){
-    console.log(`Dear Sir/ Madam`+guest_list[i]+ `\nWe are really pleased to invite you in dinner!\n\n Thank You\n`);  
- }
- let not_Present:string = "Ali";
- let new_Guest:string = "Babar Azam";
-guest_list[0]= new_Guest;
-for(let i=0; i< guest_list.length; i++){
-console.log(`Dear Sir/ Madam ` +guest_list[i]+ `\nWe are really pleased to invite you in dinner!\n\n Thank You\n`);  }
+   //Q18
+ let places:string[]= ["islamabad", "dehli","anqara"];
+ //console.log(`Original:`+ places);
+ //array in alphabatical orders.sort()
+  console.log(`copy:` +[...places].sort());
+  //printing array again in original order .reverse()
+  console.log(`copy:` + [...places].sort().reverse());
+  // Reverse the order of your list. Print the array to show that its order has changed.
+ console.log('original :' + places.sort());
 
-
-
- 
+//  Reverse the order of your list again. Print the list to show it’s back to its original order.
+ //console.log('original :' + places.sort().reverse());
+  
+ //Q20 
+ const languages : string [] = ['english','urdu','hindi','french','arabic']
+   console.log("List of languages:")
+   for(let top of languages){
+   console.log(top)}
+   
+   
